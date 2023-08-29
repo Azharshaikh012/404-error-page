097c7a6048d551219ef44fd4b6d6cba1fb23f2f8
Azhar Shaikh <sazhar012@gmail.com>
2023-08-29 17:53:00 +0530

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./dist/index.html'],
+  theme: {
+    fontFamily: {
+      'inc': ["'Inconsolata', monospace"],
+      'mont': ["'Montserrat', sans-serif"],
+      'mono': ["'Space Mono', monospace"],
+      'display': ['Oswald'],
+      'body': ['"Open Sans"'],
+    },
+    extend: {},
+  },
+  plugins: [],
+}
+
